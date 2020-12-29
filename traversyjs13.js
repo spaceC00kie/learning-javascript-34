84482c20b78d48d89bed7f8079246fe4cf9c92e2
spaceC00kie <kirstie317@gmail.com>
2020-12-28 19:47:58 -0500

    conditionals if, else, and else if

---
+++ traversyjs13.js
@@ -0,0 +1,16 @@
+//  conditionals
+
+const x = "4"
+
+if(x === 10){
+    console.log("x is 10")
+} else if(x > 10){
+    console.log("x is greater than 10")
+} else {
+    console.log("x is less than 10")
+}
+
+// "x = '10'" is not the same as "x === 10"
+//data types do not match
+//one is a string and the other is a number
+
